234bbc74c3591cd8d8805272096ab3fcb4f5f880
Anzo52 <andy@zollnersolutions.com>
2023-12-30 15:50:19 -0700

    refactor

--- main.py
+++ main.py
@@ -1,43 +1,23 @@
-import glob
 import os
-import tokenize
-
-def process_file(filepath):
-    with tokenize.open(filepath) as f:
-        tokens = list(tokenize.tokenize(f.readline))
-        new_tokens = []
-        skip_next = False
-        for token in tokens:
-            if skip_next:
-                skip_next = False
-                continue
-            if token.type == tokenize.NAME and token.string == 'print':
-                next_token = next(tokens)
-                if next_token.string != '(':
-                    new_tokens.extend([
-                        token,
-                        tokenize.TokenInfo(tokenize.OP, '(', token.start, token.end, ''),
-                        next_token,
-                        tokenize.TokenInfo(tokenize.OP, ')', next_token.start, next_token.end, '')
-                    ])
-                    skip_next = True
-                else:
-                    new_tokens.extend((token, next_token))
-            else:
-                new_tokens.append(token)
-        new_content = tokenize.untokenize(new_tokens)
-        f.seek(0)
-        f.truncate()
-        f.write(new_content)
 
 def update_print_statements(directory):
-    for filepath in glob.iglob(os.path.join(directory, '**', '*.py'), recursive=True):
-        print(filepath)
-        process_file(filepath)
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.py'):
+                filepath = os.path.join(root, file)
+                with open(filepath, 'r') as f:
+                    lines = f.readlines()
 
+                with open(filepath, 'w') as f:
+                    for line in lines:
+                        if 'print ' in line and '#' not in line:
+                            line = line.replace('print ', 'print(').rstrip() + ')\n'
+                        f.write(line)
+
+# Replace this with the path to your package
 def main():
-    directory = input("Enter the path to your package (must be absolute path): ")
+    directory = input('Enter the absolute path to your package: ')
     update_print_statements(directory)
-
-if __name__ == "__main__":
+    
+if __name__ == '__main__':
     main()
